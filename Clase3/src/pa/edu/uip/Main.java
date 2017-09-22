3a0ba4987f2aa5cab3f0ca04d95fb5144a25461a
MagoxDiaz <magalisDiaz1999@gmail.com>
2017-09-21 21:10:36 -0600

    Este es el ejemplo de java

---
+++ Clase3/src/pa/edu/uip/Main.java
@@ -0,0 +1,20 @@
+package pa.edu.uip;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  System.out.println ("Bienvenido al mundo de Java");
+	  //Tipos de Enteros
+        byte d;
+        short b;
+        int a = 0;
+        long c;
+        //Decimales
+        float e = 0.0f;
+        double: f = 0.0;
+        //Caracteres
+        char g ='a';
+        String h = "a";
+        System.out.println (a);
+    }
+}
